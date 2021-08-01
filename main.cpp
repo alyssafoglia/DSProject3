896be4938510fd6f5f36876a9206bc4d19b99bac
StuBod <stuartbodin@gmail.com>
2021-08-01 14:20:40 -0400

    No longer adds foreign titles

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 void readCSV();
 bool isFloat(string str);
+bool isAlph(string str);
 
 class Movie{
 public:
@@ -173,20 +174,48 @@ void Movie::setEditor(string _editor) {
 int main()
 {
 	readCSV();
-	std::cout << "test";
+
+	string input = "";
+	while (input != "-1") {
+		string movieTitle = "";
+		string movieActor = "";
+		string movieGenre = "";
+		Movie userInput;
+		cout << "Search by Movie (1), actor (2), or genre(3). Exit with -1 \n";
+		cin >> input;
+		switch (stoi(input)) {
+		case 1:
+			cout << "Insert Movie Title: ";
+			cin >> movieTitle;
+			// search
+			break;
+		case 2:
+			cout << "Insert an actor: ";
+			cin >> movieActor;
+			// search
+			break;
+		case 3:
+			cout << "Insert a genre: ";
+			cin >> movieGenre;
+			// search
+			break;
+		}
+	}
 }
 void readCSV()
 {
 	fstream inClean("AllMoviesDetailsCleaned.csv");
 	fstream inRaw("AllMoviesCastingRaw.csv");
-	
-	int temp = 0;
-	string line;
-	string item;
+	int count = 0; // keeps track of how many movies added.
+	int temp = 0; // keeps track of how many lines traversed.
+	string line; // used to set detailsCleaned values
+	string item; // used to set castingRaw values
 	getline(inClean, line);
 	getline(inRaw, line);
 	int diff = 0;
+
 	if (inClean.is_open() && inRaw.is_open()) {
+	// can lower conditional when testing so the entire file doesn't need to be run every time. Takes appprox 2mins 10s without adding to data structure
 	while (temp < 328842) { //328842 is max, error after 198152-> correlates to id: 314304
 		string _idC = "";
 		string _genres = "";
@@ -206,22 +235,21 @@ void readCSV()
 		string _screenplay_writer_name = "";
 		string _editor_name = "";
 		
-		//cout << line << endl << endl;
-		if(getline(inClean, line)) { // while id is < raw id
+		if(getline(inClean, line)) { // gets data from detailsCleaned.csv
 			
 			istringstream stream(line);
-			//cout << line << endl;
+			// runs through each column, but only collects some
 			getline(stream, _idC, ';');
-			getline(stream, item, ';');
+			getline(stream, item, ';'); // item is used as a placeholder for values not collected
 			getline(stream, _genres, ';');
 			getline(stream, item, ';');
 			getline(stream, item, ';');
 			getline(stream, item, ';');
 			getline(stream, item, ';');
 			getline(stream, _popularity, ';');
-			while (!isFloat(_popularity)) {
+			while (!isFloat(_popularity)) { // in some cases there is a ; not as a delimiter.
+											//This checks to see if the next value is a float as it should be instead of a string continued from the previous column
 				getline(stream, _popularity, ';');
-				//cout << _popularity << endl;
 			}
 			if (_popularity == "")
 				_popularity = "0.0";
@@ -230,7 +258,7 @@ void readCSV()
 			getline(stream, item, ';');
 			getline(stream, _revenue, ';');
 			if (_revenue == "")
-				_revenue = "0";
+				_revenue = "0"; // can cause problems otherwise when converting to an int
 			getline(stream, _runtime, ';');
 			if (_runtime == "")
 				_runtime = "0";
@@ -243,14 +271,12 @@ void readCSV()
 			getline(stream, item, ';');
 			getline(stream, item, ';');
 			getline(stream, item, ';');
-			//cout << _idC << endl;
 
 		}
 		
-		//cout << item << endl << endl;
-		if(getline(inRaw, item)) {
+		if(getline(inRaw, item)) { // gets data from castingRaw.csv
 			istringstream stream2(item);
-			//cout << item << endl;
+
 			getline(stream2, _idR, ';');
 			getline(stream2, _actor1_name, ';');
 			getline(stream2, line, ';');
@@ -270,44 +296,35 @@ void readCSV()
 			getline(stream2, line, ';');
 			getline(stream2, _screenplay_writer_name, ';');
 			getline(stream2, _editor_name, ';');
-			//cout << _idR << endl << endl;
+
 		}
 		temp++;
-		//cout << temp << endl;
-		if (_idC != _idR)
+
+		if (_idC != _idR) // should not occur. If IDs of the movies do not match then the data from both csv files will also not match
 			cout << "Clean: " << _idC << "Raw: " << _idR << endl;
 		Movie node;// = Movie(_genres, stof(_popularity), stoi(_revenue), stoi(_runtime), _title, _actor1_name, _actor2_name, _actor3_name, _actor4_name, _actor5_name, _director_name, _producer_name, _screenplay_writer_name, _editor_name);
-		cout << temp << endl;
-		cout << "Genres: " << _genres << endl;
-		cout << "Popularity: " << _popularity << endl;
-		cout << "Revenue: " << _revenue << endl;
-		cout << "Runtime: " << _runtime << endl;
-		cout << "Title: " << _title << endl;
-		cout << "Actor 1: " << _actor1_name << endl;
-		cout << "Actor 2: " << _actor2_name << endl;
-		cout << "Actor 3: " << _actor3_name << endl;
-		cout << "Actor 4: " << _actor4_name << endl;
-		cout << "Actor 5: " << _actor5_name << endl;
-		cout << "Director: " << _director_name << endl;
-		cout << "Producer: " << _producer_name << endl;
-		cout << "Screenplay by: " << _screenplay_writer_name << endl;
-		cout << "Editor: " << _editor_name << endl << endl;
-		node.setGenres(_genres);
-		node.setPop(_popularity);
-		node.setRevenue(_revenue);
-		node.setRuntime(_runtime);
-		node.setTitle(_title);
-		node.setActor1(_actor1_name);
-		node.setActor2(_actor2_name);
-		node.setActor3(_actor3_name);
-		node.setActor4(_actor4_name);
-		node.setActor5(_actor5_name);
-		node.setDirector(_director_name);
-		node.setProducer(_producer_name);
-		node.setScreenplay(_screenplay_writer_name);
-		node.setEditor(_editor_name);
-		//node.PrintMovie();
-		// Add to tree.
+
+		if (isAlph(_title) && temp !=198152 ) { // bad data at 198152
+			node.setGenres(_genres);
+			node.setPop(_popularity);
+			node.setRevenue(_revenue);
+			node.setRuntime(_runtime);
+			node.setTitle(_title);
+			node.setActor1(_actor1_name);
+			node.setActor2(_actor2_name);
+			node.setActor3(_actor3_name);
+			node.setActor4(_actor4_name);
+			node.setActor5(_actor5_name);
+			node.setDirector(_director_name);
+			node.setProducer(_producer_name);
+			node.setScreenplay(_screenplay_writer_name);
+			node.setEditor(_editor_name);
+			//node.PrintMovie();
+			// Add to tree.
+			count++;
+		}
+		//else
+			//cout << "not letter " << _title << " " << _idC << endl;
 		
 	}
 	}
@@ -315,13 +332,32 @@ void readCSV()
 
 bool isFloat(string str)
 {
-	for(int i = 0; i < str.length(); i++) {
-		if ((str[i] != '.') && (str[i] != 'E') && (str[i] != 'e') && (str[i] != ',') && (str[i] != '-') && (str[i] != '0' && str[i] != '1' && str[i] != '2' && str[i] != '3' && str[i] != '4' && str[i] != '5' && str[i] != '6' && str[i] != '7' && str[i] != '8' && str[i] != '9')) {
-			//cout << endl << "------------" << str << "--------------" << endl;
+	for(int i = 0; i < str.length(); i++) { // the non-numerical values occur if popularity is very close to 0. ex: 7.5E-5
+		if ((str[i] != '.') && (str[i] != 'E') && (str[i] != 'e') && (str[i] != ',') && (str[i] != '-') && (str[i] != '0' && str[i] != '1' 
+			&& str[i] != '2' && str[i] != '3' && str[i] != '4' && str[i] != '5' && str[i] != '6' && str[i] != '7' && str[i] != '8' && str[i] != '9')) {
 			return false; //|to do| if 'E', 'e', ',', or '-' then set to 0.0 as well
 		}
 
 	}
 	return true;
-	//cout << endl << "------------" << str << "--------------" << endl;
+}
+
+bool isAlph(string str) // isaplha() function gives error on different language scripts, so a personalized function is needed to remove them.
+{
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] != 'A' && str[i] !='a' && str[i] != 'B' && str[i] != 'b' && str[i] != 'C' && str[i] != 'c' && str[i] != 'D' && str[i] != 'd'
+			&& str[i] != 'E' && str[i] != 'e' && str[i] != 'F' && str[i] != 'f' && str[i] != 'G' && str[i] != 'g' && str[i] != 'H' && str[i] != 'h'
+			&& str[i] != 'I' && str[i] != 'i' && str[i] != 'J' && str[i] != 'j' && str[i] != 'K' && str[i] != 'k' && str[i] != 'L' && str[i] != 'l'
+			&& str[i] != 'M' && str[i] != 'm' && str[i] != 'N' && str[i] != 'n' && str[i] != 'O' && str[i] != 'o' && str[i] != 'P' && str[i] != 'p'
+			&& str[i] != 'Q' && str[i] != 'q' && str[i] != 'R' && str[i] != 'r' && str[i] != 'S' && str[i] != 's' && str[i] != 'T' && str[i] != 't'
+			&& str[i] != 'U' && str[i] != 'u' && str[i] != 'V' && str[i] != 'v' && str[i] != 'W' && str[i] != 'w' && str[i] != 'X' && str[i] != 'x'
+			&& str[i] != 'Y' && str[i] != 'y' && str[i] != 'Z' && str[i] != 'z' && str[i] != ' ' && str[i] != ':' && str[i] != ',' && str[i] != '.'
+			&& str[i] != '\'' && str[i] != '0'&& str[i] != '1' && str[i] != '2' && str[i] != '3' && str[i] != '4' && str[i] != '5' && str[i] != '6'
+			&& str[i] != '7' && str[i] != '8' && str[i] != '9' && str[i] != '!' && str[i] != '(' && str[i] != ')' && str[i] != '-' && str[i] != '&'
+			&& str[i] != '?' && str[i] != '/' && str[i] != '"'){
+			return false; 
+		}
+
+	}
+return true;
 }
